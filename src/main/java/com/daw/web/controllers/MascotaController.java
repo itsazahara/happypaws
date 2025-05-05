0c3503292b4eb56cee9bb5da876b1e6205c59799
itsazahara <aromsch1403@g.educaand.es>
2025-05-05 09:01:01 +0200

    Actualización de los controladores implementando @CrossOrigins para el correcto funcionamiento de la api en Angular

--- src/main/java/com/daw/web/controllers/MascotaController.java
+++ src/main/java/com/daw/web/controllers/MascotaController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +29,7 @@ import com.daw.services.mappers.MascotaRequestMapper;
 
 @RestController
 @RequestMapping("/mascotas")
+@CrossOrigin(origins = "http://localhost:4200")
 public class MascotaController {
 
 	@Autowired
